b636d97e42327ceb30136293043aecf3c2b98f39
King Elisha <ek.chibueze@gmail.com>
2020-01-14 11:09:32 +0100

    style

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'boxicons';
 import './App.scss';
 
 import Home from './routes/Home';
@@ -8,8 +9,14 @@ import Add from './routes/Add';
 function App() {
   return (
     <Router>
-      <Route path="/" component={Home} exact />
-      <Route path="/add" component={Add} exact />
+      <div className="container">
+        <header>
+          <h1><box-icon name='map-pin' animation='tada' size="2em" ></box-icon> <span>My Locator</span></h1>
+        </header>
+
+        <Route path="/" component={Home} exact />
+        <Route path="/add" component={Add} exact />
+      </div>
     </Router>
   );
 }
