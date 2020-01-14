18d45a2f882bb5d59bbf4cd9b0479f1baab3f7bb
King Elisha <ek.chibueze@gmail.com>
2020-01-14 12:57:42 +0100

    add store

--- client/src/routes/Add.js
+++ client/src/routes/Add.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
 
 export default class Add extends Component {
+  state = {
+    id: '',
+    address: ''
+  };
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  addStore = async (e) => {
+    e.preventDefault();
+
+    const { id, address } = this.state;
+    if (id === '' || address === '')
+      return alert('All fields are required');
+    const newLocation = {
+      id,
+      address
+    };
+
+    const res = await fetch('/api/v1/locations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newLocation)
+    });
+
+    if (res.status === 400) {
+      // store exists
+      return alert('Store already exists');
+    }
+
+    alert('Store added');
+    this.props.history.replace('/');
+  };
+
   render() {
     return (
       <div>
-        <h1>Add</h1>
+        <form onSubmit={this.addStore}>
+          <input
+            type="text"
+            name="id"
+            placeholder="location id"
+            onChange={this.onChange} />
+
+          <input
+            type="text"
+            name="address"
+            placeholder="address"
+            onChange={this.onChange} />
+
+          <button type="submit">Add</button>
+        </form>
       </div>
     );
   }
