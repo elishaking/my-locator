b78c2a54e8eef70a60feb17fc86ee9441184c608
King Elisha <ek.chibueze@gmail.com>
2020-01-14 12:11:45 +0100

    display locations on map

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -6,16 +6,66 @@ export default class Home extends Component {
   componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1Ijoia2luZ2VsaXNoYSIsImEiOiJjazVkb3lubGswY2loM29vMzVkNWV1a3Y5In0.nkxgiiw4liuz4rDm4GgJEw';
 
+    this.getLocations();
+  }
+
+  getLocations = async () => {
+    const res = await fetch('/api/v1/locations');
+    const data = await res.json();
+
+    const locations = data.data.map((location) => {
+      const { coordinates } = location.location;
+      return {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [
+            coordinates[0],
+            coordinates[1]
+          ]
+        },
+        'properties': {
+          'id': location.id,
+          'icon': 'shop'
+        }
+      };
+    });
+
+    this.loadMap(locations);
+  };
+
+  loadMap = (locations) => {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11'
     });
-  }
+
+    map.on('load', () => {
+      map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': {
+          'type': 'geojson',
+          'data': {
+            'type': 'FeatureCollection',
+            'features': locations
+          }
+        },
+        layout: {
+          'icon-image': '{icon}-15',
+          'icon-size': 1.5,
+          'text-field': '{id}',
+          'text-offset': [0, .9],
+          'text-anchor': 'top'
+        }
+      });
+    });
+  };
 
   render() {
     return (
       <div>
-        <Link to="/add">Add Location</Link>
+        <Link className="btn" to="/add">Add Location</Link>
 
         <div id="map" style={{
           width: "100%",
