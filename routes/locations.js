4201b2085644cb0f561b5ed4396ababde29d06d3
King Elisha <ek.chibueze@gmail.com>
2020-01-14 09:05:51 +0100

    refactored

--- routes/locations.js
+++ routes/locations.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { getLocations, addLocation } = require('../controllers/locations');
 
-router.get('/', getLocations);
-router.post('/', addLocation);
+router.route('/')
+  .get(getLocations)
+  .post(addLocation);
 
 module.exports = router;
