327111e3522e3ae126ad130f6ea04f37bc83ea1f
King Elisha <ek.chibueze@gmail.com>
2020-01-15 12:57:31 +0100

    added response-util

--- src/services/locations.js
+++ src/services/locations.js
@@ -1,12 +1,25 @@
 const Location = require('../models/location');
+const ResponseUtil = require('../utils/response');
 
 const findLocations = () => new Promise((resolve, reject) => {
   Location.find()
-    .then((locations) => resolve(locations))
+    .then((locations) => resolve(ResponseUtil
+      .createResponseData(
+        true,
+        200,
+        locations.length === 0 ? "Locations Empty" : "Locations found",
+        locations
+      )
+    ))
     .catch((err) => {
       console.error(err);
 
-      reject(err);
+      reject(ResponseUtil
+        .createResponseData(
+          false,
+          500,
+          "Server Error"
+        ));
     });
 });
 
